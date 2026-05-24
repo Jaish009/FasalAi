89f75d1c9ce5ed4f6858dcf5c878bf10cca47872
Jaish009 <jaishsayeedsiddiki@gmail.com>
2026-05-24 10:53:41 +0530

    style: center authentication pages

--- fasalai-nextjs/src/app/sign-up/[[...sign-up]]/page.tsx
+++ fasalai-nextjs/src/app/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex h-screen w-full items-center justify-center bg-gray-50">
+    <div style={{ display: "flex", height: "100vh", width: "100%", alignItems: "center", justifyContent: "center", backgroundColor: "#fefae0" }}>
       <SignUp />
     </div>
   );
